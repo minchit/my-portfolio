41df499e19107af88a4a8b2d93fcaf98fbaa9371
minchit <minchitstar@gmail.com>
2025-03-14 22:42:01 +0700

    version 1 of my portfolio

---
+++ src/content/profileData.ts
@@ -0,0 +1,147 @@
+export const PROFILE = {
+  site: {
+    SEO: {
+      //     more complicated description for SEO
+      title: "Min Chit - Learn, Share and Grow",
+      description:
+        "Turning Errors into Features – Tech Enthusiast | Implementation Engineer | Cloud Explorer | Application Support | Aspiring Data Engineer | Multi Cloud | Mission Critical Systems.",
+    },
+  },
+  timezone: "Asia/Bangkok",
+  keywords: "Tech, Data Engineer, Cloud, AWS, Azure, Portfolio, Min Chit",
+  language: "en-US",
+  firstName: "Min Chit Paing Thein",
+  name: "minch-IT",
+  headLine: "Learn, Share and Grow ❤️",
+  
+  headLine2: "Turning Errors into Features  – Tech Enthusiast | Implementation Engineer | Cloud Explorer |",
+  
+  industry_headline: "Industries I've Worked in",
+  industry_headline2: "Yes, I've worked across multiple industries. Why? Because I love exploring different business domains, understanding how applications power real-world workflows, and diving into the technology that drives them. From handling millions of banking transactions to ensuring telecom billing runs smoothly, I enjoy knowing that the systems I support impact millions of users daily.",
+  website: "https://minchit.com",
+  repo: "https://github.com/minchit/",
+  avatarUrl: "https://portfolios-pic-mc8834.s3.ap-southeast-7.amazonaws.com/mcpt_profile.jpg",
+  shortIntros: [
+    "A tech enthusiast with a passion for solving complex problems, implementing scalable systems, and occasionally debugging myself after too much coffee. With a background in FinTech, Telecom, and Cloud Technologies, I’ve worked with enterprise-level applications, banking transactions, and telecom billing platforms. Whether it’s system deployments, troubleshooting mission-critical services, or making sense of logs that look like alien languages, I’m here for the challenge!",
+    "Yes, I've worked across multiple industries. Why? Because I love exploring different business domains, understanding how applications power real-world workflows, and diving into the technology that drives them. From handling millions of banking transactions to ensuring telecom billing runs smoothly, I enjoy knowing that the systems I support impact millions of users daily.",
+  ],
+  words: [
+    "My tech journey began in the trenches of IT troubleshooting, where every issue felt urgent, and every fix had a countdown. Working with mission-critical systems, I tackled transaction failures in banking platforms, ensuring every payment processed correctly instead of disappearing into the void. In telecom, I safeguarded billing system stability, knowing that even a minute of downtime could mean massive revenue loss. Through it all, I quickly learned that technology loves to break—especially at 2 AM.",
+    
+    "Now, I’m diving into cloud computing (AWS & Azure), IoT, and data engineering—because ensuring systems run smoothly is just as important as building them to be resilient from the start. My goal? To bridge automation, scalability, and innovation while keeping ‘unexpected errors’ where they belong—in my logs, not in production.",
+],
+  skills: [
+    "Python",
+    "SQL",
+    "AWS",
+    "Databricks",
+    "Cloud Computing",
+    "Azure",
+    "ETL Pipelines",
+    "Astro",
+  ],
+
+  db_skills: [
+    "SQL",
+    "PLSQL",
+    "MySQL",
+    "Oracle",
+    "Postgres",
+    "Databricks",
+    "Python",
+  ],
+
+  aws_skills: [
+    "EC2",
+    "Lambda",
+    "S3",
+    "CloudWatch",
+    "RDS",
+    "VPS",
+  ],
+
+  azure_skills: [
+    "Azure Data Lake",
+    "Azure Data Factory",
+  ],
+
+  sys_skills: [
+    "Red Hat",
+    "CentOS",
+    "Weblogic",
+    "JBoss",
+  ],
+
+  dev_skills: [
+    "Python",
+    "Java",
+    "Astro",
+    "HTML",
+    "CSS",
+    "JavaScript",
+  ],
+
+  pjm_skills: [
+    "Jira",
+    "Confluence",
+    "HP ITSM",
+  ],
+
+
+  industries: [
+    "Banking & Financial Tech",
+    "Telecommunications",
+    "Software Development",
+  ],
+  links: {
+    github: "https://github.com/minchit",
+    linkedin: "https://www.linkedin.com/in/min-chit-paing-thein",
+
+    facebook: "https://www.facebook.com/minchitpthein/",
+
+  },
+  studies: [
+    // studies, qualifications and certifications
+    {
+      title: "Bachelor of Computer Science",
+      level: "Degree",
+      institution: "University of Computer Studies, Yangon",
+    },
+    {
+      title: "AWS Certified Solution Architect Associate",
+      institution: "Amazon Web Services",
+      href: "#",
+      level: "Professional Certification",
+    },
+    {
+      title: "Microsoft Certified: Azure Data Fundamentals",
+      institution: "Microsoft",
+      href: "https://learn.microsoft.com/en-us/users/minchit/transcript/dzmr4h85mnxw4o9?tab=tab-learning-paths",
+      level: "Professional Certification",
+    },
+    {
+      title: "Academy Accreditation - Databricks Fundamentals",
+      institution: "Databricks",
+      href: "https://credentials.databricks.com/2ec88b24-1649-4dda-8fb8-f85773ed6ac5#acc.JIguLyiT",
+      level: "Professional Certification",
+    },
+    {
+      title: "Microsoft Azure for Data Engineering",
+      institution: "Coursera",
+      href: "https://www.coursera.org/account/accomplishments/verify/V70TTLDOND3R",
+      level: "Certification",
+    },
+    {
+      title: "Discovering Quantam Computing",
+      institution: "Udacity",
+      href: "https://www.udacity.com/certificate/e/4646c8a0-40e2-11ef-aa07-9364806e9ab8",
+      level: "Certification",
+    },
+    {
+      title: "Foundation of Project Management",
+      institution: "Coursera",
+      href: "https://www.coursera.org/account/accomplishments/records/FBN2ADKF6TGX",
+      level: "Certification",
+    },
+  ],
+};
